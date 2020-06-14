f565913693f8a75881ecea7a3789a84d23b8e1dd
ShreyeeDobhal <66896591+ShreyeeDobhal@users.noreply.github.com>
2020-06-14 01:00:18 -1200

    Add files via upload
    I've tried visualizing data through bar charts,area plots,pie charts,box plots and other visualtion tools by extracting data from a data set.
    I've used  libraries like panadas, numpy etc to created data frame, and implementing other operations, to modify data.

---
+++ datavisualization-Copy1.py
@@ -0,0 +1,581 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np  # useful for many scientific computing in Python
+import pandas as pd # primary data structure library
+
+
+# In[3]:
+
+
+df_can = pd.read_excel('https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/DV0101EN/labs/Data_Files/Canada.xlsx',
+                       sheet_name='Canada by Citizenship',
+                       skiprows=range(20),
+                       skipfooter=2
+                      )
+
+print('Data downloaded and read into a dataframe!')
+
+
+# In[4]:
+
+
+print(df_can.shape)
+
+
+# In[5]:
+
+
+df_can.drop(['AREA', 'REG', 'DEV', 'Type', 'Coverage'], axis=1, inplace=True);
+
+# let's view the first five elements and see how the dataframe was changed
+df_can.head()
+
+
+# In[6]:
+
+
+df_can.rename(columns={'OdName':'Country', 'AreaName':'Continent','RegName':'Region'}, inplace=True)
+
+# let's view the first five elements and see how the dataframe was changed
+df_can.head()
+
+
+# In[7]:
+
+
+df_can.columns = list(map(str, df_can.columns))
+
+# let's check the column labels types now
+all(isinstance(column, str) for column in df_can.columns)
+
+
+# In[8]:
+
+
+#### 4. Set the country name as index - useful for quickly looking up countries using .loc method.
+df_can.set_index('Country', inplace=True)
+
+# let's view the first five elements and see how the dataframe was changed
+df_can.head()
+
+
+# In[9]:
+
+
+#### 5. Add total column.
+df_can['Total'] = df_can.sum(axis=1)
+
+# let's view the first five elements and see how the dataframe was changed
+df_can.head()
+
+
+# In[10]:
+
+
+print ('data dimensions:', df_can.shape)
+
+
+# In[11]:
+
+
+# finally, let's create a list of years from 1980 - 2013
+# this will come in handy when we start plotting the data
+years = list(map(str, range(1980, 2014)))
+years
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+mpl.style.use('ggplot') # optional: for ggplot-like style
+
+# check for latest version of Matplotlib
+print ('Matplotlib version: ', mpl.__version__) # >= 2.0.0
+df_can.sort_values(['Total'], ascending=False, axis=0, inplace=True)
+
+# get the top 5 entries
+df_top5 = df_can.head()
+
+# transpose the dataframe
+df_top5 = df_top5[years].transpose() 
+
+df_top5.head()
+
+
+# In[12]:
+
+
+df_top5.index = df_top5.index.map(int) # let's change the index values of df_top5 to type integer for plotting
+df_top5.plot(kind='area', 
+             stacked=False,
+             figsize=(20, 10), # pass a tuple (x, y) size
+             )
+
+plt.title('Immigration Trend of Top 5 Countries')
+plt.ylabel('Number of Immigrants')
+plt.xlabel('Years')
+
+plt.show()
+
+
+# In[13]:
+
+
+df_top5.plot(kind='area', 
+             alpha=0.25, # 0-1, default value a= 0.5
+             stacked=False,
+             figsize=(20, 10),
+            )
+
+plt.title('Immigration Trend of Top 5 Countries')
+plt.ylabel('Number of Immigrants')
+plt.xlabel('Years')
+
+plt.show()
+
+
+# In[14]:
+
+
+### Two types of plotting
+#1
+
+df_top5.plot(kind='area', alpha=0.35, figsize=(20, 10)) 
+plt.title('Immigration trend of top 5 countries')
+plt.ylabel('Number of immigrants')
+plt.xlabel('Years')
+    
+    
+
+
+# In[15]:
+
+
+# option 2: preferred option with more flexibility
+
+ax = df_top5.plot(kind='area', alpha=0.35, figsize=(20, 10))
+ax.set_title('Immigration Trend of Top 5 Countries')
+ax.set_ylabel('Number of Immigrants')
+ax.set_xlabel('Years')
+
+
+# In[16]:
+
+
+#HISTOGRAM REPRESENTATION:-
+#let's quickly view the 2013 data
+df_can['2013'].head()
+
+
+# In[17]:
+
+
+# np.histogram returns 2 values
+count, bin_edges = np.histogram(df_can['2013'])
+
+print(count) # frequency count
+print(bin_edges) # bin ranges, default = 10 bins
+
+
+# In[18]:
+
+
+# 'bin_edges' is a list of bin intervals
+count, bin_edges = np.histogram(df_can['2013'])
+
+df_can['2013'].plot(kind='hist', figsize=(8, 5), xticks=bin_edges)
+
+plt.title('Histogram of Immigration from 195 countries in 2013') # add a title to the histogram
+plt.ylabel('Number of Countries') # add y-label
+plt.xlabel('Number of Immigrants') # add x-label
+
+plt.show()
+
+
+# In[19]:
+
+
+# let's quickly view the dataset 
+df_can.loc[['Denmark', 'Norway', 'Sweden'], years]
+
+
+# In[20]:
+
+
+# generate histogram
+df_can.loc[['Denmark', 'Norway', 'Sweden'], years].plot.hist()
+
+
+# In[21]:
+
+
+#That does not look right! 
+
+#Don't worry, you'll often come across situations like this when creating plots.
+#The solution often lies in how the underlying dataset is structured.
+
+#Instead of plotting the population frequency distribution of the population for the 3 countries, *pandas* instead plotted 
+#the population frequency distribution for the `years`.
+
+#This can be easily fixed by first transposing the dataset, and then plotting as shown below.
+# transpose dataframe
+df_t = df_can.loc[['Denmark', 'Norway', 'Sweden'], years].transpose()
+df_t.head()
+
+
+# In[22]:
+
+
+df_t.plot(kind='hist', figsize=(10, 6))
+
+plt.title('Histogram of Immigration from Denmark, Norway, and Sweden from 1980 - 2013')
+plt.ylabel('Number of Years')
+plt.xlabel('Number of Immigrants')
+
+plt.show()
+
+
+# In[23]:
+
+
+#Let's make a few modifications to improve the impact and aesthetics of the previous plot:
+#* increase the bin size to 15 by passing in `bins` parameter
+#* set transparency to 60% by passing in `alpha` paramemter
+#* label the x-axis by passing in `x-label` paramater
+#* change the colors of the plots by passing in `color` parameter
+# let's get the x-tick values
+count, bin_edges = np.histogram(df_t, 15)
+
+# un-stacked histogram
+df_t.plot(kind ='hist', 
+          figsize=(10, 6),
+          bins=15,
+          alpha=0.6,
+          xticks=bin_edges,
+          color=['coral', 'darkslateblue', 'mediumseagreen']
+         )
+
+plt.title('Histogram of Immigration from Denmark, Norway, and Sweden from 1980 - 2013')
+plt.ylabel('Number of Years')
+plt.xlabel('Number of Immigrants')
+
+plt.show()
+
+
+# In[24]:
+
+
+import matplotlib
+for name, hex in matplotlib.colors.cnames.items():
+    print(name, hex)
+
+
+# In[25]:
+
+
+#If we do no want the plots to overlap each other, we can stack them using the `stacked` paramemter.
+count, bin_edges = np.histogram(df_t, 15)
+xmin = bin_edges[0] - 10   #  first bin value is 31.0, adding buffer of 10 for aesthetic purposes 
+xmax = bin_edges[-1] + 10  #  last bin value is 308.0, adding buffer of 10 for aesthetic purposes
+
+# stacked Histogram
+df_t.plot(kind='hist',
+          figsize=(10, 6), 
+          bins=15,
+          xticks=bin_edges,
+          color=['coral', 'darkslateblue', 'mediumseagreen'],
+          stacked=True,
+          xlim=(xmin, xmax)
+         )
+plt.title('Histogram of Immigration from Denmark, Norway, and Sweden from 1980 - 2013')
+plt.ylabel('Number of Years')
+plt.xlabel('Number of Immigrants') 
+
+plt.show()
+
+
+# In[26]:
+
+
+#bar charts
+# step 1: get the data
+df_iceland = df_can.loc['Iceland', years]
+df_iceland.head()
+
+
+# In[27]:
+
+
+# step 2: plot data
+df_iceland.plot(kind='bar', figsize=(10, 6))
+
+plt.xlabel('Year') # add to x-label to the plot
+plt.ylabel('Number of immigrants') # add y-label to the plot
+plt.title('Icelandic immigrants to Canada from 1980 to 2013') # add title to the plot
+
+plt.show()
+
+
+# In[28]:
+
+
+df_iceland.plot(kind='bar', figsize=(10, 6), rot=90) 
+
+plt.xlabel('Year')
+plt.ylabel('Number of Immigrants')
+plt.title('Icelandic Immigrants to Canada from 1980 to 2013')
+
+# Annotate arrow
+plt.annotate('',                      # s: str. will leave it blank for no text
+             xy=(32, 70),             # place head of the arrow at point (year 2012 , pop 70)
+             xytext=(28, 20),         # place base of the arrow at point (year 2008 , pop 20)
+             xycoords='data',         # will use the coordinate system of the object being annotated 
+             arrowprops=dict(arrowstyle='->', connectionstyle='arc3', color='blue', lw=2)
+            )
+
+# Annotate Text
+plt.annotate('2008 - 2011 Financial Crisis', # text to display
+             xy=(28, 30),                    # start the text at at point (year 2008 , pop 30)
+             rotation=72.5,                  # based on trial and error to match the arrow
+             va='bottom',                    # want the text to be vertically 'bottom' aligned
+             ha='left',                      # want the text to be horizontally 'left' algned.
+            )
+
+plt.show()
+
+
+# In[29]:
+
+
+#PIE CHARTS
+# group countries by continents and apply sum() function 
+df_continents = df_can.groupby('Continent', axis=0).sum()
+
+# note: the output of the groupby method is a `groupby' object. 
+# we can not use it further until we apply a function (eg .sum())
+print(type(df_can.groupby('Continent', axis=0)))
+
+df_continents.head()
+
+
+# In[30]:
+
+
+# autopct create %, start angle represent starting point
+df_continents['Total'].plot(kind='pie',
+                            figsize=(5, 6),
+                            autopct='%1.1f%%', # add in percentages
+                            startangle=90,     # start angle 90° (Africa)
+                            shadow=True,       # add shadow      
+                            )
+
+plt.title('Immigration to Canada by Continent [1980 - 2013]')
+plt.axis('equal') # Sets the pie chart to look like a circle.
+
+plt.show()
+
+
+# In[31]:
+
+
+colors_list = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue', 'lightgreen', 'pink']
+explode_list = [0.1, 0, 0, 0, 0.1, 0.1] # ratio for each continent with which to offset each wedge.
+
+df_continents['Total'].plot(kind='pie',
+                            figsize=(15, 6),
+                            autopct='%1.1f%%', 
+                            startangle=90,    
+                            shadow=True,       
+                            labels=None,         # turn off labels on pie chart
+                            pctdistance=1.12,    # the ratio between the center of each pie slice and the start of the text generated by autopct 
+                            colors=colors_list,  # add custom colors
+                            explode=explode_list # 'explode' lowest 3 continents
+                            )
+
+# scale the title up by 12% to match pctdistance
+plt.title('Immigration to Canada by Continent [1980 - 2013]', y=1.12) 
+
+plt.axis('equal') 
+
+# add legend
+plt.legend(labels=df_continents.index, loc='upper left') 
+
+plt.show()
+
+
+# In[32]:
+
+
+#BOX CHART
+# to get a dataframe, place extra square brackets around 'Japan'.
+df_japan = df_can.loc[['Japan'], years].transpose()
+df_japan.head()
+
+
+# In[33]:
+
+
+df_japan.plot(kind='box', figsize=(8, 6))
+
+plt.title('Box plot of Japanese Immigrants from 1980 - 2013')
+plt.ylabel('Number of Immigrants')
+
+plt.show()
+df_japan.describe()
+
+
+# In[34]:
+
+
+df_CI= df_can.loc[['China', 'India'], years].transpose()
+df_CI.head()
+
+fig = plt.figure() # create figure
+
+ax0 = fig.add_subplot(1, 2, 1) # add subplot 1 (1 row, 2 columns, first plot)
+ax1 = fig.add_subplot(1, 2, 2) # add subplot 2 (1 row, 2 columns, second plot). See tip below**
+
+# Subplot 1: Box plot
+df_CI.plot(kind='box', color='blue', vert=False, figsize=(20, 6), ax=ax0) # add to subplot 1
+ax0.set_title('Box Plots of Immigrants from China and India (1980 - 2013)')
+ax0.set_xlabel('Number of Immigrants')
+ax0.set_ylabel('Countries')
+
+# Subplot 2: Line plot
+df_CI.plot(kind='line', figsize=(20, 6), ax=ax1) # add to subplot 2
+ax1.set_title ('Line Plots of Immigrants from China and India (1980 - 2013)')
+ax1.set_ylabel('Number of Immigrants')
+ax1.set_xlabel('Years')
+
+plt.show()
+
+
+# In[35]:
+
+
+#SCATTER PLOT:
+# we can use the sum() method to get the total population per year
+df_tot = pd.DataFrame(df_can[years].sum(axis=0))
+
+# change the years to type int (useful for regression later on)
+df_tot.index = map(int, df_tot.index)
+
+# reset the index to put in back in as a column in the df_tot dataframe
+df_tot.reset_index(inplace = True)
+
+# rename columns
+df_tot.columns = ['year', 'total']
+
+# view the final dataframe
+df_tot.head()
+
+
+# In[36]:
+
+
+df_tot.plot(kind='scatter', x='year', y='total', figsize=(10, 6), color='darkblue')
+
+plt.title('Total Immigration to Canada from 1980 - 2013')
+plt.xlabel('Year')
+plt.ylabel('Number of Immigrants')
+
+plt.show()
+
+
+# In[37]:
+
+
+#for best fit line:
+x = df_tot['year']      # year on x-axis
+y = df_tot['total']     # total on y-axis
+fit = np.polyfit(x, y, deg=1)
+
+fit
+
+
+# In[38]:
+
+
+df_tot.plot(kind='scatter', x='year', y='total', figsize=(10, 6), color='darkblue')
+
+plt.title('Total Immigration to Canada from 1980 - 2013')
+plt.xlabel('Year')
+plt.ylabel('Number of Immigrants')
+
+# plot line of best fit
+plt.plot(x, fit[0] * x + fit[1], color='red') # recall that x is the Years
+plt.annotate('y={0:.0f} x + {1:.0f}'.format(fit[0], fit[1]), xy=(2000, 150000))
+
+plt.show()
+
+# print out the line of best fit
+'No. Immigrants = {0:.0f} * Year + {1:.0f}'.format(fit[0], fit[1]) 
+
+
+# In[39]:
+
+
+#bubble plot
+df_can_t = df_can[years].transpose() # transposed dataframe
+
+# cast the Years (the index) to type int
+df_can_t.index = map(int, df_can_t.index)
+
+# let's label the index. This will automatically be the column name when we reset the index
+df_can_t.index.name = 'Year'
+
+# reset index to bring the Year in as a column
+df_can_t.reset_index(inplace=True)
+
+# view the changes
+df_can_t.head()
+
+
+# In[40]:
+
+
+# normalize Brazil data
+norm_brazil = (df_can_t['Brazil'] - df_can_t['Brazil'].min()) / (df_can_t['Brazil'].max() - df_can_t['Brazil'].min())
+
+# normalize Argentina data
+norm_argentina = (df_can_t['Argentina'] - df_can_t['Argentina'].min()) / (df_can_t['Argentina'].max() - df_can_t['Argentina'].min())
+
+
+# In[41]:
+
+
+# Brazil
+ax0 = df_can_t.plot(kind='scatter',
+                    x='Year',
+                    y='Brazil',
+                    figsize=(14, 8),
+                    alpha=0.5,                  # transparency
+                    color='green',
+                    s=norm_brazil * 2000 + 10,  # pass in weights 
+                    xlim=(1975, 2015)
+                   )
+
+# Argentina
+ax1 = df_can_t.plot(kind='scatter',
+                    x='Year',
+                    y='Argentina',
+                    alpha=0.5,
+                    color="blue",
+                    s=norm_argentina * 2000 + 10,
+                    ax = ax0
+                   )
+
+ax0.set_ylabel('Number of Immigrants')
+ax0.set_title('Immigration from Brazil and Argentina from 1980 - 2013')
+ax0.legend(['Brazil', 'Argentina'], loc='upper left', fontsize='x-large')
+
+
+# In[42]:
+
+
+import seaborn as sns;
+ax=sns.regplot()
+
